704796b2e1da79d74d890763fcf2a0057dc52d60
Paola <paolareategui@hotmail.com>
2026-08-03 16:29:10 +1000

    Complete 1.4 (Use an array of objects)
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- fso_1.1_1.14/src/App.jsx
+++ fso_1.1_1.14/src/App.jsx
@@ -5,24 +5,25 @@ import Total from './components/total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts =  [ {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
 
   return (
     <div>
       <Header course = {course} />
-      <Content part1 = {part1.name} exercises1 = {part1.exercises1} part2 = {part2.name} exercises2 = {part2.exercises2} part3 = {part3.name} exercises3 = {part3.exercises3}/>
-      <Total exercises1 = {part1.exercises1} exercises2 = {part2.exercises2} exercises3 = {part3.exercises3}/>
+      <Content parts = {parts}/>
+      <Total parts = {parts}/>
     </div>
   )
 }
