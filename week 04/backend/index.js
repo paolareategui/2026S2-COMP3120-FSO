b7f21cbc2fbbd658e3616b9b53c2301b4261f326
Paola <paolareategui@hotmail.com>
2026-08-24 14:05:01 +1000

    Add post request to add new persons. Add error checking
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- week 04/backend/index.js
+++ week 04/backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 
 let persons = [
   {
@@ -46,7 +48,7 @@ app.get('/api/persons/:id', (request, response) => {
   const id = request.params.id
   const person = persons.find(p => p.id === id)
   if (person) {
-    response.json(note)
+    response.json(person)
   } else {
     response.status(404).end()
   }
@@ -59,10 +61,40 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 app.post('/api/persons/', (request, response) => {
-  const personId =
-    persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => Number(n.id)))
+    : 0
+  const person = request.body
+  person.id = String(maxId + 1)
+
+  if (!person.name) {
+    return response.status(400).json({
+      error: "the name is missing"
+    })
+  }
+  if (!person.number) {
+    return response.status(400).json({
+      error: "the number is missing"
+    })
+  }
+
+  const alreadyExists = persons.find(p => p.name.toLowerCase() === person.name.toLowerCase())
+
+  if (alreadyExists) {
+    return response.status(400).json({
+      error: "this person is already on the phone book"
+    })
+  }
+
+  else {
+    persons = persons.concat(person)
+    return response.json(person)
+  }
+
+
 })
+
+
 
 
 
