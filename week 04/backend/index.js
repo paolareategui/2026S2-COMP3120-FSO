f13628449ecc17c2feba0a48785612f190c23e89
Paola <paolareategui@hotmail.com>
2026-08-24 14:56:09 +1000

    Add CORS
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- week 04/backend/index.js
+++ week 04/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 app.use(express.json())
+app.use(cors())
 
 
 let persons = [
@@ -98,7 +100,7 @@ app.post('/api/persons/', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
