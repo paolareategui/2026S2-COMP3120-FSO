0423cb7f03dde35db57d21e11c8a194ef0fa3cab
Paola <paolareategui@hotmail.com>
2026-08-25 21:00:42 +1000

    Add validation for phone number
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- week 04/backend/models/person.js
+++ week 04/backend/models/person.js
@@ -17,11 +17,20 @@ mongoose.connect(url, { family: 4 })
     })
 
 const personSchema = new mongoose.Schema({
-    name: { type: String,
-    minLength: 3,
-    required: true
-  },
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            }
+        }
+    }
 })
 
 personSchema.set('toJSON', {
