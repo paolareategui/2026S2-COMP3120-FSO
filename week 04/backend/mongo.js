db9d8c6da613fa69de314dc2cabd750b0cc847ea
Paola <paolareategui@hotmail.com>
2026-08-25 17:08:50 +1000

    Add and retrieve data from MongoDB
    Signed-off-by: Paola <paolareategui@hotmail.com>

---
+++ week 04/backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://paolaquetal_db_user:${password}@cluster0.ehbbcpx.mongodb.net`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+if (process.argv.length > 3) {
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+else {
+
+    Person.find({}).then(result => {
+                    console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+
+}
+
