9b85012e3edb1a73853fb626bd73fd1c00b56940
Paola <paolareategui@hotmail.com>
2026-09-01 11:36:14 +1000

    refactor schema
    Signed-off-by: Paola <paolareategui@hotmail.com>

--- week 06/blog_backend/index.js
+++ week 06/blog_backend/index.js
@@ -1,40 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-mongoose.connect(url, { family: 4 })
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 app.use(express.json())
+
+const Blog = require('./models/post')
 
 app.get('/api/blogs', (request, response) => {
   Blog.find({}).then((blogs) => {
---
+++ week 06/blog_backend/models/post.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+mongoose.connect(url, { family: 4 })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+module.exports = mongoose.model('Blog', blogSchema)
